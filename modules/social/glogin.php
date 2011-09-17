44de8f34e0df36dd54172ee93f7e6e9a83d8a83a
tutei <admin@tutei.com>
2011-09-17 10:49:39 -0300

    google login - first working version

--- modules/social/glogin.php
+++ modules/social/glogin.php
@@ -1,30 +1,58 @@
 <?php
+
 //session_destroy();exit;
+
+function genRandomString() {
+    $length = 10;
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz!@#$';
+    $string = '';
+
+    for ($p = 0; $p < $length; $p++) {
+        $string .= $characters[mt_rand(0, strlen($characters))];
+    }
+
+    return $string;
+}
+
+function loadImageCURL($source, $save_to) {
+    $ch = curl_init($source);
+    $fp = fopen($save_to, "wb");
+
+// set URL and other appropriate options
+    $options = array(CURLOPT_FILE => $fp,
+        CURLOPT_HEADER => 0,
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_TIMEOUT => 60); // 1 minute timeout (should be enough)
+
+    curl_setopt_array($ch, $options);
+
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+}
+
+$Module = $Params['Module'];
+
 session_start();
 
 $ini = eZINI::instance('googleapi.ini');
 $client = new apiClient();
-$client->setApplicationName("Google+ PHP Starter Application");
+$client->setApplicationName("GoogleLogin");
 $client->setClientId($ini->variable("APIAccess", "ClientId"));
 $client->setClientSecret($ini->variable("APIAccess", "ClientSecret"));
 //$client->setDeveloperKey($ini->variable("APIAccess", "APIKey"));
-echo $ini->variable("APIAccess", "APIKey");
+
 $ini = eZINI::instance('site.ini');
 $client->setRedirectUri(('http://' . $ini->variable("SiteSettings", "SiteURL") . '/social/glogin/'));
 
 //$client->setScopes(array('https://www.googleapis.com/auth/plus.me'));
 $plus = new apiBuzzService($client);
 
-if (isset($_REQUEST['logout'])) {
-    unset($_SESSION['access_token']);
-}
-
-
-
 if (isset($_GET['code'])) {
+
     $client->authenticate();
+
     $_SESSION['access_token'] = $client->getAccessToken();
-    //header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']);
 }
 
 
@@ -37,20 +65,54 @@ if (isset($_SESSION['access_token'])) {
 
 if ($client->getAccessToken()) {
 
-    $me = $plus->people->get('@me');
-    var_dump($me);
+    unset($_SESSION['access_token']);
+    try {
+        $me = $plus->people->get('@me');
+    } catch (Exception $e) {
+        echo 'Error: not public profile';
+        eZExecution::cleanExit();
+    }
+    $email = null;
+    foreach ($me['emails'] as $m) {
+        echo $m["primary"];
+        if ($m["primary"])
+            $email = $m["value"];
+    }
 
-    /*
-    $optParams = array('maxResults' => 100);
-    $activities = $plus->activities->listActivities('@me', '@public', $optParams);
 
-    // The access token may have been updated lazily.
-     * 
-     */
-    $_SESSION['access_token'] = $client->getAccessToken();
+    $user = eZUser::fetchByEmail($email);
+
+    if ($user instanceof eZUser) {
+
+        eZUser::setCurrentlyLoggedInUser($user, $user->attribute('contentobject_id'));
+        $Module->redirectTo('/');
+    } else {
+
+        $user = new ezpObject('user', 12, 14, 2);
+        $name = explode(' ', $me["displayName"]);
+        $first_name = $name[0];
+        $last_name = implode(' ', array_slice($input, 0, 1));
+
+        $username = $email;
+        $password = genRandomString();
+
+        $image = 'var/cache/content/' . $password;
+        loadImageCURL($me['thumbnailUrl'], $image);
+
+        $user->__set("first_name", $first_name);
+        $user->__set("last_name", $last_name);
+        $user->__set("user_account", "$username|$username|" . md5("$username\n$pass") . "|2");
+        $user->__set("image", $image);
+
+        $user->publish();
+
+        unlink($image);
+        $user = eZUser::fetchByEmail($email);
+        eZUser::setCurrentlyLoggedInUser($user, $user->attribute('contentobject_id'));
+        $Module->redirectTo('/');
+    }
 } else {
     $http = eZHTTPTool::instance();
     $http->redirect($client->createAuthUrl());
 }
-eZExecution::cleanExit();
 ?>
